92fa4570879e395e06a19b9b1b3878c5903c9aa1
isadorateles <isadoratelesdecastro@gmail.com>
2020-06-24 16:45:01 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi")
+// console.log("hi")
+
+// SERVER CODE
+
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
