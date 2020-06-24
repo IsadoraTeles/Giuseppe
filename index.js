9ff8758b28b5ac8bb685637bf0acc63bd5057b97
Isadora Teles <isadoratelesdecastro@gmail.com>
2020-06-24 19:50:16 +0200

    update

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 
+app.post('/api', (request, response)=>{
+    console.log(request);
+}); // my application interface for my clients to send data to me
+
 const port = process.env.PORT || 3000;
 
 
